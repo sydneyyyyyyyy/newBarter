29478e6dab405581378ed3802356855638392fc0
sydneyyyyyyyy <121756643+sydneyyyyyyyy@users.noreply.github.com>
2023-01-02 10:03:26 +0500

    Update Api.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-export const url ='http://127.0.0.1:8000';
+export const url = 'http://alpha@azure.com';
 
 export const axiosSignin = async (formData) => await axios.post(`${url}/api/auth/login`, formData);
 export const axiosSignup = async (formData) => await axios.post(`${url}/api/auth/signup`, formData);
